86525ff829940b2deca14020175136c9cfe085e6
space-shell <contact@j-n.me.uk>
2018-11-12 00:35:36 +0000

    Massive chamges

--- src/zya.js
+++ src/zya.js
@@ -1,6 +1,88 @@
 // NOTE - JN - ℤ !== Z
 
-const Zya = (Component, methods) => class extends Component {
+// TODO - JN - Zya constructor to differentiate between Async functions, Class constructore and functions / Objects
+
+const RESOLVE = null
+
+const STREAM = []
+
+const ARCHIVE = []
+
+const PROCESSES = []
+
+const NODES = []
+
+const STREAMER = {
+	async * [Symbol.asyncIterator] () {
+		while (true) {
+			while(STREAM.length)
+				yield STREAM.shift()
+
+			await new Promise(resolve => RESOLVE = resolve)
+
+			RESOLVE = null
+		}
+	}
+}
+
+const dispatch = (action = {}, route = true) => {
+	RESOLVE && RESOLVE()
+
+	if (Object.keys(action).length !== 0 && action.constructor === Object)
+		action.origin = this['ℤ']
+
+	STREAM.push({ ...action, route })
+}
+
+const stream = async function * (data) {
+	for await(const { route, origin, ...obj } of data) {
+		if (origin === this['ℤ'])
+			ARCHIVE.push({ [origin || 'process']: { ...obj, route } })
+
+		if (route || (route === false && origin === this['ℤ']))
+			yield * Object.keys(obj).map(key => {
+				if (this[key])
+					try {
+						const backStream = this[key](obj[key]) || { [key]: obj[key] }
+
+						return {
+							...backStream,
+							origin,
+							route
+						}
+					} catch (e) {
+						console.log(e)
+
+						return { [key]: obj[key], origin, route }
+					}
+				else
+					return { [key]: obj[key], origin, route }
+			})
+		else
+			yield { ...obj, origin, route }
+	}
+}
+
+const use = (...args) => {
+	// TODO - JN - Assign unique identifier to process, eg: Constructor name
+
+	Zya.PROCESSES.push(...args)
+
+	return Zya.restart()
+}
+
+const restart = async () => {
+	const preprocessors = PROCESSES.map(proc => stream => proc(stream, dispatch))
+
+	const process = [ ...preprocessors, ...NODES ]
+		.reduce((stream, f) => f(stream), STREAMER)
+
+	for await(const i of process);
+
+	return
+}
+
+const generateClass = (base, methods) => class extends base {
 	constructor () {
 		super()
 
@@ -9,106 +91,24 @@ const Zya = (Component, methods) => class extends Component {
 			.toString(36)
 			.substring(2)
 
-		Object.keys(methods).forEach(method => {
-			if (typeof methods[method] === 'function')
-				this[method] = methods[method]
-		})
+		if (methods)
+			Object.keys(methods).forEach(method => {
+				if (typeof methods[method] === 'function')
+					this[method] = methods[method]
+			})
 
-		Zya.NODES.push(this.stream.bind(this))
+		NODES.push(this.stream.bind(this))
 
 		Zya.restart()
 	}
-
-	$dispatch (action, route = true) {
-		Zya.RESOLVE && Zya.RESOLVE()
-
-		if (Object.keys(action).length !== 0 && action.constructor === Object)
-			Object.assign(action, { origin: this['ℤ'] })
-
-		Zya.STREAM.push({ ...action, route })
-	}
-
-	async * stream (data) {
-		for await(const { route, origin, ...obj } of data) {
-			if (origin === this['ℤ'])
-				Zya.ARCHIVE.push({ [origin || 'process']: { ...obj, route } })
-
-			if ( route || (route === false && origin === this['ℤ']) )
-				yield * Object.keys(obj).map(key => {
-
-					if (this[key])
-						try {
-							const backStream = this[key](obj[key]) || { [key]: obj[key] }
-
-							return {
-								...backStream,
-								origin,
-								route
-							}
-						} catch (e) {
-							console.log(e)
-
-							return { [key]: obj[key], origin, route }
-						}
-					else
-						return { [key]: obj[key], origin, route }
-				})
-			else
-				yield { ...obj, origin, route }
-		}
-	}
 }
 
+export default function Zya (base, methods) {
+	console.log(base)
 
-Object.assign(Zya, {
-	use (...args) {
-		// TODO - JN - Assign unique identifier to process, eg: Constructor name
-	
-		Zya.PROCESSES.push(...args)
+	if (typeof base === 'function' && base instanceof HTMLElement)
+		generateClass(base, methods)
 
-		return Zya.restart()
-	},
-
-	async restart () {
-		const preprocessors = Zya.PROCESSES.map(proc => stream => proc(stream, Zya.dispatch))
-
-		const process = [ ...preprocessors, ...Zya.NODES ]
-			.reduce((stream, f) => f(stream), Zya.STREAMER)
-
-		for await(const i of process);
-
-		return
-	},
-
-	dispatch (action) {
-		Zya.RESOLVE && Zya.RESOLVE()
-
-		Zya.STREAM.push({ ...action, route: true })
-	},
-
-	RESOLVE: null,
-
-	STREAM: [],
-
-	ARCHIVE: [],
-
-	PROCESSES: [],
-
-	NODES: [],
-
-	STREAMER: {
-		async * [Symbol.asyncIterator] () {
-			while (true) {
-				while(Zya.STREAM.length)
-					yield Zya.STREAM.shift()
-
-				await new Promise(resolve => Zya.RESOLVE = resolve)
-
-				Zya.RESOLVE = null
-			}
-		}
-
-	}
-})
-
-export default Zya
+	if (typeof Zya === 'function' && base.constructer.name === 'AsyncGeneratorFunction')
+		generatorIterator(base, methods)
+}
